b5539c6f2548137f579d0c5acb38b0852d1f0d2b
yashgaur1805 <91069334+yashgaur1805@users.noreply.github.com>
2022-06-21 21:48:46 +0530

    Create Encryption_and_decryption_of_a_grade.java

---
+++ Encryption_and_decryption_of_a_grade.java
@@ -0,0 +1,16 @@
+public class Encryption_and_decryption_of_a_grade {
+
+	public static void main(String[] args) {
+		//Encryption of a grade.
+		char grade='B';
+		grade=(char)(grade+8);
+		System.out.println(grade);
+		//decryption of a grade.
+		grade=(char)(grade-8);
+		System.out.println(grade);
+		
+	
+
+	}
+
+}
