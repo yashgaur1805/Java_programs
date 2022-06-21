04c1025a118db5ddfbd4ae5366c8a4e2de86bdfd
yashgaur1805 <91069334+yashgaur1805@users.noreply.github.com>
2022-06-21 21:59:04 +0530

    Create Using_comparison_operator.java

---
+++ Using_comparison_operator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Using_comparison_operator {
+
+	public static void main(String[] args) {
+		System.out.println("Enter a Number");
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		System.out.println(a>9);
+		
+
+	}
+
+}
